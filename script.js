333d95736371c0b5f6577be4bde62adeb470d96d
Ciril Joy <145466031+Ciril-Joy@users.noreply.github.com>
2025-04-16 16:31:36 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+// You can add JavaScript for interactive elements
+// For example, smooth scrolling to sections
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
